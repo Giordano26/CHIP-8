3a66a8b77947ca76ab3f23970b7c341d40301ffc
¡Giordano26 <smfgiordano@gmail.com>
2025-03-12 23:01:33 -0300

    feat: stack and funcs implemented

--- core/chip8.go
+++ core/chip8.go
@@ -3,9 +3,30 @@ package core
 import (
 	"github.com/Giordano26/chip8/core/memory"
 	"github.com/Giordano26/chip8/core/registers"
+	"github.com/Giordano26/chip8/core/stack"
 )
 
 type Chip8 struct {
 	Chip8Memory    memory.Memory
 	Chip8Registers registers.Registers
+	Chip8Stack     stack.Stack
+}
+
+func stackInBounds(chip8 *Chip8) {
+	if chip8.Chip8Registers.SP >= stack.StackSize {
+		panic("[WARNING] Stack pointer out of bounds")
+	}
+}
+
+func StackPush(chip8 *Chip8, value uint16) {
+	stackInBounds(chip8)
+
+	chip8.Chip8Stack.Stack[chip8.Chip8Registers.SP] = value
+	chip8.Chip8Registers.SP += 1
+}
+
+func StackPop(chip8 *Chip8) uint16 {
+
+	chip8.Chip8Registers.SP -= 1
+	return chip8.Chip8Stack.Stack[chip8.Chip8Registers.SP]
 }
---
+++ core/stack/stack.go
@@ -0,0 +1,9 @@
+package stack
+
+const (
+	StackSize = 16
+)
+
+type Stack struct {
+	Stack [StackSize]uint16
+}
